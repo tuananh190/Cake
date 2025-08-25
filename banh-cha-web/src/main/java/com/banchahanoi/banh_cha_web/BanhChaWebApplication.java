d37682134304076e600c2a45f55052561c6b419d
Lưu Tuấn Anh <139638161+tuananh190@users.noreply.github.com>
2025-08-25 09:25:26 +0700

    Initial commit

---
+++ banh-cha-web/src/main/java/com/banchahanoi/banh_cha_web/BanhChaWebApplication.java
@@ -0,0 +1,13 @@
+package com.banchahanoi.banh_cha_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BanhChaWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BanhChaWebApplication.class, args);
+	}
+
+}
